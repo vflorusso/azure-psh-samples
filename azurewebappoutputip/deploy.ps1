92e9538a60c70c360b0f0c4ac1fdf7d1ed59ffee
Vito Flavio Lorusso <vf.lorusso@live.com>
2016-11-22 09:28:23 +0100

    add azure web app with output ip

---
+++ azurewebappoutputip/deploy.ps1
@@ -0,0 +1,20 @@
+﻿
+Login-AzureRmAccount
+
+$SubscriptionName = "[Azure-subscription]"
+Select-AzureRmSubscription -SubscriptionNAme $SubscriptionName
+
+$RGName = "[webapprg]"
+$location = "West Europe"
+$templatefile = "webappdeploy.json"
+$templateparameters = "webappdeploy.parameters.json"
+
+
+
+# Create a Resource Group
+New-AzureRmResourceGroup -Name $RGName -Location $location
+ 
+# Deploy the Template to the Resource Group
+New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName `
+                                   -TemplateFile $templatefile `
+                                   -TemplateParameterFile $templateparameters
